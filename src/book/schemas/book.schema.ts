45a7e7a9393ef59be35661b0fea1e2bd9b394708
adarista012 <ad.arista012@gmail.com>
2024-08-24 00:29:39 -0400

    fc

---
+++ src/book/schemas/book.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+
+@Schema({
+    timestamps: true
+})
+
+export class Book {
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    price: number;
+
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book)
