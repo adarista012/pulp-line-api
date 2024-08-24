158c5dc16c31e71ce1e688f1d1c02c2b37abddcd
adarista012 <ad.arista012@gmail.com>
2024-08-24 00:31:52 -0400

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: '.env',
+    //   isGlobal: true,
+    // }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     BookModule
     ],
